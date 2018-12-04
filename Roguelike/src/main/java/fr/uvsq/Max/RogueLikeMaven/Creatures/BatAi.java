982a4dd644733471bce767180bb0011f7e1d99ba
BenGuillot <ben.guyot.guillot@gmail.com>
2018-12-04 22:26:46 +0100

    jaruc

--- Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/BatAi.java
+++ Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/BatAi.java
@@ -1,7 +1,7 @@
 package fr.uvsq.Max.RogueLikeMaven.Creatures;
 
 import fr.uvsq.Max.RogueLikeMaven.World.Tile;
-import fr.uvsq.Max.RogueLikeMaven.World.Line;
+
 import fr.uvsq.Max.RogueLikeMaven.World.Point;
 
 public class BatAi extends CreatureAi {

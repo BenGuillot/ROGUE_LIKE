b0a892d3acf32ee4d5f5a3a8a4f3996a4b022629
Laureline Martin <laureline.martin@ens.uvsq.fr>
2018-12-04 13:31:42 +0100

    Mouvements de créatures.
    Ajout des méthodes onEnter(int, int, int, Tile) et wander() pour les mouvements des créatures.

--- Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/CreatureAi.java
+++ Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/CreatureAi.java
@@ -12,7 +12,21 @@ public class CreatureAi {
         this.creature.setCreatureAi(this);
     }
 
+    public void wander() {
+    	int rx = (int)(Math.random()*3)-1;
+    	int ry = (int)(Math.random()*3)-1;
+    	this.creature.moveBy(rx, ry, 0);
+    }
+    
     public void onEnter(int x, int y, int z, Tile tile){
+    	if(tile.isGrond()) {
+    		this.creature.x = x;
+    		this.creature.y = y;
+    		this.Creature.z = z;
+    	}
+    	else {
+    		this.creature.doAction("Bump into a wall");
+    	}
     }
 
     public void onUpdate(){

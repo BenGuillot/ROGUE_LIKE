59007b6696b789bc6837757370377d5b14aa0799
Laureline Martin <laureline.martin@ens.uvsq.fr>
2018-12-04 13:42:48 +0100

    Creation de la classe BatAi
    Cette classe est une sous-classe de CreatureAi

---
+++ Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/BatsAi.java
@@ -0,0 +1,9 @@
+package fr.uvsq.Max.RogueLikeMaven.Creatures;
+
+import fr.uvsq.Max.RogueLikeMaven.World.Tile;
+import fr.uvsq.Max.RogueLikeMaven.World.Line;
+import fr.uvsq.Max.RogueLikeMaven.World.Point;
+
+public class BatsAi extends CreatureAi {
+	
+}

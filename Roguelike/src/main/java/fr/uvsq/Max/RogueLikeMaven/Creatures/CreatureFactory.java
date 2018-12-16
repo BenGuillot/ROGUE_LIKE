1dc28d8d454606ef122a26a941ef3e8929ac9670
Utilisateur <user@debian.arcahe.ovh>
2018-12-16 11:48:18 +0100

    s

--- Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/CreatureFactory.java
+++ Roguelike/src/main/java/fr/uvsq/Max/RogueLikeMaven/Creatures/CreatureFactory.java
@@ -41,7 +41,7 @@ public class CreatureFactory {
     }
    
     public Creature newSuperPnj(int depth){
-        Creature pnj = new Creature(world, 'P', AsciiPanel.white, 1, 0, 1, 0);
+        Creature pnj = new Creature(world, '*', AsciiPanel.white, 1, 0, 1, 0);
         world.addAtEmptyLocation(pnj, depth);
         new PnjAi(pnj, Chapeau(0));
         return pnj;
